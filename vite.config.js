6e3fe6633aa3a1e7535ad86f0eda6358a8779ee1
bhaskar_bisht <bisht3182568@gmail.com>
2024-06-06 17:47:14 +0530

    first chatting app

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 10000 || 5173,
-    host: '0.0.0.0'
-  }
+  base: "/vite-react-deploy/"
 })
